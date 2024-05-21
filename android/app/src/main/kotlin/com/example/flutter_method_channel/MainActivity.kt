94402c5cac9664604ccc13b52e7752eb7bc77120
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2024-05-21 11:56:44 +0700

    commit method channel - android

---
+++ android/app/src/main/kotlin/com/example/flutter_method_channel/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.flutter_method_channel
+
+import android.app.AlertDialog
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "samples.flutter.dev/battery"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+                call, result ->
+            // This method is invoked on the main thread.
+            // TODO
+
+            if (call.method == "getBatteryLevel") {
+                val batteryLevel = getBatteryLevel()
+
+                if (batteryLevel != -1) {
+                    result.success(batteryLevel)
+                } else {
+                    result.error("UNAVAILABLE", "Battery level not available.", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+//            getPopupSnackbar()
+        }
+    }
+
+    private fun getBatteryLevel(): Int {
+        val batteryLevel: Int
+        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+        }
+
+        return batteryLevel
+    }
+
+    /// FOR GET POPUP
+    fun getPopupSnackbar() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Popup Dialog")
+        builder.setMessage("This is an AlertDialog.")
+
+        builder.setPositiveButton("OK") { dialog, _ ->
+            // Do something when OK is clicked
+            dialog.dismiss()
+        }
+
+        builder.setNegativeButton("Cancel") { dialog, _ ->
+            // Do something when Cancel is clicked
+            dialog.dismiss()
+        }
+
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+}
